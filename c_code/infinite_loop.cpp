7fef932f395d832d2821cc9835fa951a82e6873f
charles <mcrichard.buaa@gmail.com>
2025-02-09 20:11:20 -0800

    c code

--- c_code/infinite_loop.cpp
+++ c_code/infinite_loop.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main() {
+  int a = 0;
+  
   for (int i = 0; i < 10000000; i++) {
-    cout << "good" << endl;
+    // cout << "good" << endl;
+    int b = a++;
   }
 }
